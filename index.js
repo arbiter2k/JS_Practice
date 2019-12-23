b02e6a09818f84d9b04f382953f0d0aec97b369e
arbiter2k <rehan.arbiter@gmail.com>
2019-12-23 19:14:44 +0530

    using pop ups

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-console.time("click time");
-alert("Useless Interruption");
-console.timeLog("click time");
-alert("Just Click It");
-console.timeEnd("click time");
+window.alert("This is an alert. Call 911. Haha XD");
+if(confirm("You are walking into uncharted waters")){
+    document.writeln("Congratulations you have drowned.");
+    document.writeln("P.S. You can\'t walk on water.");
+}
+else{
+    document.writeln("Huh, Smart Guy.\n You have survived for now.");
+}
+var answer=prompt("Are you okay?","Yes or No");
+if(answer==="yes" || answer==="Yes"){
+    document.writeln("No you are not.");
+}
+else{
+    document.writeln("Yes you definitely are not");
+}
