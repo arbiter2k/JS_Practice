e62fddc445bd7ee96334671e8c3ab28f73e68837
arbiter2k <rehan.arbiter@gmail.com>
2019-12-23 19:36:34 +0530

    using pop ups version 2

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-window.alert("This is an alert. Call 911. Haha XD");
-if(confirm("You are walking into uncharted waters")){
-    document.writeln("Congratulations you have drowned.");
-    document.writeln("P.S. You can\'t walk on water.");
-}
-else{
-    document.writeln("Huh, Smart Guy.\n You have survived for now.");
-}
-var answer=prompt("Are you okay?","Yes or No");
+var answer=prompt("Are you human?","Yes or No");
 if(answer==="yes" || answer==="Yes"){
-    document.writeln("No you are not.");
+    document.write("Fine you can enter.");
 }
 else{
     document.writeln("Yes you definitely are not");
 }
+//window.alert("This is an alert. Call 911. Haha XD");
+if(confirm("Wanna play a game?")){
+    if (confirm("You are walking into uncharted waters")) {
+         document.write("Congratulations you have drowned.<br>");
+         document.write("P.S. You can\'t walk on water.<br>");
+     }
+    else {
+         document.write("Huh, Smart Guy.<br> You have survived for now.<br>");
+    }
+}
+
