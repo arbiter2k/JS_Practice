6a4abc10391c948edc945d4c9574f67166da3b9d
arbiter2k <rehan.arbiter@gmail.com>
2020-03-26 19:47:20 +0530

    mouse hover test

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ var click= document.querySelector("#click");
 click.addEventListener("click",function(){
     body.classList.toggle("clicked");
 });
+click.addEventListener("mouseover",function(){
+    click.classList.add("hover");
+});
+click.addEventListener("mouseleave",function(){
+    click.classList.remove("hover");
+});
