162ca27402dd3a0e42c8c74e9e6aff383db2387d
arbiter2k <rehan.arbiter@gmail.com>
2020-03-26 18:58:54 +0530

    DOM Manipulation

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-var answer=prompt("Are you human?","Yes or No");
-if(answer==="yes" || answer==="Yes"){
-    document.write("Fine you can enter.");
-}
-else{
-    document.writeln("Yes you definitely are not");
-}
-//window.alert("This is an alert. Call 911. Haha XD");
-if(confirm("Wanna play a game?")){
-    if (confirm("You are walking into uncharted waters")) {
-         document.write("Congratulations you have drowned.<br>");
-         document.write("P.S. You can\'t walk on water.<br>");
-     }
-    else {
-         document.write("Huh, Smart Guy.<br> You have survived for now.<br>");
-    }
-}
-
+var id= document.querySelector("li");
+console.log("working");
+id.classList.toggle("blue");
