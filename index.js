9c834c6d17783204ca0372856222d3828ab54533
arbiter2k <rehan.arbiter@gmail.com>
2020-03-28 14:45:07 +0530

    To-Do List Version 1

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-var body=document.querySelector("body");
-var click= document.querySelector("#click");
-click.addEventListener("click",function(){
-    body.classList.toggle("clicked");
+var checkbox= document.getElementsByTagName("input");
+var reset= document.getElementById("reset");
+var lis=document.getElementsByTagName("li");
+function strikethrough(arg){
+    if(arg.checked===true)
+            arg.parentNode.classList.add("clicked");  
+    else
+            arg.parentNode.classList.remove("clicked");
+}
+for(var i=0; i<lis.length ; i++){
+    checkbox[i].addEventListener("click",function(){
+        strikethrough(this)
+    });
+}
+reset.addEventListener("mouseover",function(){
+    this.classList.add("hover");
 });
-click.addEventListener("mouseover",function(){
-    click.classList.add("hover");
+reset.addEventListener("mouseout",function(){
+    this.classList.remove("hover");
 });
-click.addEventListener("mouseleave",function(){
-    click.classList.remove("hover");
-});
+reset.addEventListener("click",function(){
+    for(i=0; i<checkbox.length; i++){
+         checkbox[i].parentNode.classList.remove("clicked");
+         checkbox[i].checked=false;
+    }
+}); 
